a62cb6e60539f5c23d00b3d79ae29522a5c0e469
M.Taimoor Hassan <mtaimoor.tbox@gmail.com>
2023-03-08 20:32:21 +0500

    Started working on StroyTelling Ap
    Will be completed til firiday.

---
+++ StoryTellingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryTellingApp
+//
+//  Created by Muhammad Taimoor Hassan on 08/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
